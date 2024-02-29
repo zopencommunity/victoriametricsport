b8df5e3112d0fd046675ec9d083d5a0318b41ae4
Mackenzie Malainey <mac.malainey@ibm.com>
2024-02-28 21:43:31 -0500

    add vmagent port

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/victoriametricsport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/victoriametricsport/actions/workflows/bump.yml)
+
+# victoriametrics
+
+a fast, cost-effective and scalable monitoring solution and time series database.[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/victoriametricsport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/victoriametricsport.git'), string(name: 'PORT_DESCRIPTION', value: 'a fast, cost-effective and scalable monitoring solution and time series database.[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
